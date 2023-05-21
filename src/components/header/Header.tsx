68a1ba0042e40128d5a73fd71775c8876e5fd489
Weronika <WerRic96@protonmail.com>
2023-05-21 22:25:07 +0200

    Render hamburger menu based on screen size

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,27 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import "./header.css";
 import menu from "../../icons/menu-icon.svg";
 
 export const Header: FC = () => {
-  return (
-    <header>
-      <h1>Trivia</h1>
-    </header>
-  );
+  const [width, setWidth] = useState(window.innerWidth);
+  const [mobileNavVisible, setMobileNavVisible] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+  }, []);
+
+  if (width < 800) {
+    return (
+      <header>
+        <h1>Trivia</h1>
+        <img src={menu} alt="hamburger menu" className="menu" />
+      </header>
+    );
+  } else {
+    return (
+      <header>
+        <h1>Trivia</h1>
+      </header>
+    );
+  }
 };
