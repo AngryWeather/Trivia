08318a80a656d52529807081217896da68c40937
Weronika <WerRic96@protonmail.com>
2023-05-21 21:26:21 +0200

    Install prettier

--- src/RouteSwitch.tsx
+++ src/RouteSwitch.tsx
@@ -3,9 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 export const RouteSwitch: FC = () => {
-    return (
-        <BrowserRouter basename="/trivia">
-            <App></App>
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter basename="/trivia">
+      <App></App>
+    </BrowserRouter>
+  );
+};
