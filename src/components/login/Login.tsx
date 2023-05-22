b1569cfb6bf8bef9d0c66265d0f13b17208b0144
Weronika <WerRic96@protonmail.com>
2023-05-23 00:04:23 +0200

    Set up basic login component

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,9 @@
+import { FC, useState } from "react";
+
+export const Login: FC = () => {
+  const [username, setUsername] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  return <div></div>;
+};
