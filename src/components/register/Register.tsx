f600087a3cc5beb9286ad4238f0622b1a2ce68a1
Weronika <WerRic96@protonmail.com>
2023-05-26 00:28:43 +0200

    Set username and password on change

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -1,13 +1,19 @@
 import { FC, useState } from "react";
 
 export const Register: FC = () => {
+  const [username, setUsername] = useState<string>();
   const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
 
   return (
     <section id="register">
       <form>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input
+          onChange={(e) => (e.target as HTMLInputElement).value}
+          id="username"
+          type="text"
+        ></input>
         <label htmlFor="email">email</label>
         <input
           onChange={(e) => setEmail((e.target as HTMLInputElement).value)}
@@ -15,7 +21,11 @@ export const Register: FC = () => {
           id="email"
         ></input>
         <label htmlFor="password">password</label>
-        <input type="password" id="password"></input>
+        <input
+          onChange={(e) => (e.target as HTMLInputElement).value}
+          type="password"
+          id="password"
+        ></input>
         <button type="submit">Submit</button>
       </form>
     </section>
