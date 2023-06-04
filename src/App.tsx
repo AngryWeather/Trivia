8e92403d492eac8f99185b0b159582cd6cc0d1fc
Weronika <WerRic96@protonmail.com>
2023-06-04 23:54:46 +0200

    Add login route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/header/Header";
 import { Register } from "./components/register/Register";
+import { Login } from "./components/login/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/user/register" element={<Register />}></Route>
+        <Route path="/user/login" element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
