6072a9436662c8b28d9ead48d19d6cb2de01e71e
Weronika <WerRic96@protonmail.com>
2023-06-04 23:51:11 +0200

    Add form to Login component

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,9 +1,18 @@
 import { FC, useState } from "react";
 
 export const Login: FC = () => {
-  const [username, setUsername] = useState<string>();
   const [email, setEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
 
-  return <div></div>;
+  return (
+    <div className="form" id="login">
+      <form aria-label="form" action="">
+        <label htmlFor="email-login">Email</label>
+        <input type="email" name="email-login" id="email-login" />
+        <div className="line"></div>
+        <label htmlFor="password-login">Password</label>
+        <input type="password" name="password-login" id="password-login" />
+      </form>
+    </div>
+  );
 };
