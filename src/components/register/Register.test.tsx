fd659f835d614bc026e394e8510fbbc7725f8559
Weronika <WerRic96@protonmail.com>
2023-06-04 21:17:47 +0200

    Remove unused imports

--- src/components/register/Register.test.tsx
+++ src/components/register/Register.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Register } from "./Register";
 import { MemoryRouter } from "react-router-dom";
 
