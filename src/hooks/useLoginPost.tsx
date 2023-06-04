a4373cd6e0a742818336bb49767f3caeae7e23a7
Weronika <WerRic96@protonmail.com>
2023-06-05 00:08:54 +0200

    Create useLoginPost hook

---
+++ src/hooks/useLoginPost.tsx
@@ -0,0 +1,33 @@
+import { SyntheticEvent, useState } from "react";
+
+type Data = {
+  email: string | undefined;
+  password: string | undefined;
+};
+
+export const usePost = ({ email, password }: Data) => {
+  const [responseStatus, setResponseStatus] = useState<number>();
+  const [responseBody, setResponseBody] = useState<any>();
+
+  async function handleSubmit(e: SyntheticEvent): Promise<void> {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8080/user/login", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    setResponseStatus(response.status);
+    try {
+      setResponseBody(await response.json());
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return { handleSubmit, responseStatus, responseBody };
+};
