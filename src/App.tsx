976334b8ebc8348e1c08f875cefac67dd97848a8
Weronika <WerRic96@protonmail.com>
2023-06-07 01:08:10 +0200

    Pass currentUser to provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,24 @@ import "./App.css";
 import { Header } from "./components/header/Header";
 import { Register } from "./components/register/Register";
 import { Login } from "./components/login/Login";
+import { useState } from "react";
+import { UserContext } from "./contexts/UserContext";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({
+    username: null,
+  });
+
   return (
-    <div className="App">
-      <Header></Header>
-      <Routes>
-        <Route path="/user/register" element={<Register />}></Route>
-        <Route path="/user/login" element={<Login></Login>}></Route>
-      </Routes>
-    </div>
+    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+      <div className="App">
+        <Header></Header>
+        <Routes>
+          <Route path="/user/register" element={<Register />}></Route>
+          <Route path="/user/login" element={<Login></Login>}></Route>
+        </Routes>
+      </div>
+    </UserContext.Provider>
   );
 }
 
