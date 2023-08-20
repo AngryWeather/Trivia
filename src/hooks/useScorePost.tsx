901ef3b6975d5685a4ff21cfacfcd71c282b15ab
Weronika <WerRic96@protonmail.com>
2023-08-20 22:20:32 +0200

    Fix useScorePost hook

--- src/hooks/useScorePost.tsx
+++ src/hooks/useScorePost.tsx
@@ -1,6 +1,29 @@
+import { SyntheticEvent, useState } from "react";
+
 type Data = {
   username: string;
   score: number;
 };
 
-export const useScorePost = ({ username, score }: Data) => {};
+export const useScorePost = ({ username, score }: Data) => {
+  const [responseStatus, setResponseStatus] = useState<number>();
+
+  async function handleSubmit(e: SyntheticEvent) {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:8080/trivia/score", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        score,
+      }),
+    });
+    setResponseStatus(response.status);
+  }
+
+  return { handleSubmit, responseStatus };
+};
